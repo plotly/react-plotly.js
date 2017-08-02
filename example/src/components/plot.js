956054e4020ce631931198ec4c28cf1af5ff790a
Ricky Reusser <rsreusser@gmail.com>
2017-08-01 18:15:02 -0700

    File cleanup

--- example/src/components/plot.js
+++ example/src/components/plot.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './plot.css'
 
-import createPlotly from '../../../src'
+import createPlotly from '../../../'
 const PlotlyComponent = createPlotly(Plotly)
 
 
