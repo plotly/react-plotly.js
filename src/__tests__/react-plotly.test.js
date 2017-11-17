4ebad6f18e52c4f3c624016ad118d6a0b17f1f2b
bpostlethwaite <post.ben.here@gmail.com>
2017-11-17 13:45:55 -0500

    lint react-plotly.js.test

--- src/__tests__/react-plotly.test.js
+++ src/__tests__/react-plotly.test.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { mount } from "enzyme";
-import createComponent from "../factory";
-import once from "onetime";
+import React from 'react';
+import createComponent from '../factory';
+import once from 'onetime';
+import {mount} from 'enzyme';
 
-describe("<Plotly/>", () => {
+describe('<Plotly/>', () => {
   let Plotly, PlotComponent;
 
   function createPlot(props) {
@@ -35,9 +35,9 @@ describe("<Plotly/>", () => {
     );
   }
 
-  describe("with mocked plotly.js", () => {
+  describe('with mocked plotly.js', () => {
     beforeEach(() => {
-      Plotly = require.requireMock("../__mocks__/plotly.js").default;
+      Plotly = require.requireMock('../__mocks__/plotly.js').default;
       PlotComponent = createComponent(Plotly);
 
       // Override the parent element size:
@@ -47,8 +47,8 @@ describe("<Plotly/>", () => {
       });
     });
 
-    describe("initialization", function() {
-      it("calls Plotly.newPlot on instantiation", done => {
+    describe('initialization', function() {
+      it('calls Plotly.newPlot on instantiation', done => {
         createPlot({})
           .then(() => {
             expect(Plotly.newPlot).toHaveBeenCalled();
@@ -59,42 +59,42 @@ describe("<Plotly/>", () => {
           .then(done);
       });
 
-      it("passes data", done => {
+      it('passes data', done => {
         createPlot({
-          data: [{ x: [1, 2, 3] }],
-          layout: { title: "foo" },
+          data: [{x: [1, 2, 3]}],
+          layout: {title: 'foo'},
         })
           .then(() => {
             expectPlotlyAPICall(Plotly.newPlot, {
-              data: [{ x: [1, 2, 3] }],
-              layout: { title: "foo" },
+              data: [{x: [1, 2, 3]}],
+              layout: {title: 'foo'},
             });
           })
           .catch(err => done.fail(err))
           .then(done);
       });
 
-      it("accepts width and height", done => {
+      it('accepts width and height', done => {
         createPlot({
-          layout: { width: 320, height: 240 },
+          layout: {width: 320, height: 240},
         })
           .then(() => {
             expectPlotlyAPICall(Plotly.newPlot, {
-              layout: { width: 320, height: 240 },
+              layout: {width: 320, height: 240},
             });
           })
           .catch(err => done.fail(err))
           .then(done);
       });
 
-      it("overrides the height when fit: true", done => {
+      it('overrides the height when fit: true', done => {
         createPlot({
-          layout: { width: 320, height: 240 },
+          layout: {width: 320, height: 240},
           fit: true,
         })
           .then(() => {
             expectPlotlyAPICall(Plotly.newPlot, {
-              layout: { width: 320, height: 240 },
+              layout: {width: 320, height: 240},
             });
           })
           .catch(err => done.fail(err))
@@ -102,42 +102,42 @@ describe("<Plotly/>", () => {
       });
     });
 
-    describe("plot updates", () => {
-      it("updates data", done => {
+    describe('plot updates', () => {
+      it('updates data', done => {
         createPlot({
           fit: true,
-          layout: { width: 123, height: 456 },
+          layout: {width: 123, height: 456},
           onUpdate: once(() => {
             expectPlotlyAPICall(Plotly.newPlot, {
-              data: [{ x: [1, 2, 3] }],
-              layout: { width: 123, height: 456 },
+              data: [{x: [1, 2, 3]}],
+              layout: {width: 123, height: 456},
             });
             done();
           }),
         })
           .then(plot => {
-            plot.setProps({ data: [{ x: [1, 2, 3] }] });
+            plot.setProps({data: [{x: [1, 2, 3]}]});
           })
           .catch(err => done.fail(err));
       });
 
-      it("sets the title", done => {
+      it('sets the title', done => {
         createPlot({
           fit: false,
           onUpdate: once(() => {
             expectPlotlyAPICall(Plotly.newPlot, {
-              layout: { title: "test test" },
+              layout: {title: 'test test'},
             });
             done();
           }),
         })
           .then(plot => {
-            plot.setProps({ layout: { title: "test test" } });
+            plot.setProps({layout: {title: 'test test'}});
           })
           .catch(err => done.fail(err));
       });
 
-      it("clears adds event handlers on every newPlot", done => {
+      it('clears adds event handlers on every newPlot', done => {
         let wrapper;
         createPlot({
           fit: false,
@@ -150,19 +150,19 @@ describe("<Plotly/>", () => {
           .then(plot => {
             wrapper = plot;
             wrapper.instance().clearLocalEventHandlers = jest.fn();
-            expect(Object.keys(wrapper.instance().handlers)).toEqual(["Click"]);
-            plot.setProps({ layout: { title: "test test" } });
+            expect(Object.keys(wrapper.instance().handlers)).toEqual(['Click']);
+            plot.setProps({layout: {title: 'test test'}});
           })
           .catch(err => done.fail(err));
       });
     });
 
-    describe("responding to window events", () => {
-      describe("with fit: true", () => {
-        it("does not call relayout on initialization", done => {
+    describe('responding to window events', () => {
+      describe('with fit: true', () => {
+        it('does not call relayout on initialization', done => {
           createPlot({
             fit: true,
-            onRelayout: () => done.fail("Unexpected relayout event"),
+            onRelayout: () => done.fail('Unexpected relayout event'),
           })
             .then(() => {
               setTimeout(done, 20);
@@ -170,7 +170,7 @@ describe("<Plotly/>", () => {
             .catch(err => done.fail(err));
         });
 
-        it("calls relayout on window resize when fit: true", done => {
+        it('calls relayout on window resize when fit: true', done => {
           let relayoutCnt = 0;
           createPlot({
             fit: true,
@@ -182,17 +182,17 @@ describe("<Plotly/>", () => {
             },
           })
             .then(() => {
-              window.dispatchEvent(new Event("resize"));
+              window.dispatchEvent(new Event('resize'));
             })
             .catch(err => done.fail(err));
         });
       });
 
-      describe("with fit: false", () => {
-        it("does not call relayout on init", done => {
+      describe('with fit: false', () => {
+        it('does not call relayout on init', done => {
           createPlot({
             fit: false,
-            onRelayout: () => done.fail("Unexpected relayout event"),
+            onRelayout: () => done.fail('Unexpected relayout event'),
           })
             .then(() => {
               setTimeout(done, 20);
@@ -200,13 +200,13 @@ describe("<Plotly/>", () => {
             .catch(err => done.fail(err));
         });
 
-        it("does not call relayout on window resize", done => {
+        it('does not call relayout on window resize', done => {
           createPlot({
             fit: false,
-            onRelayout: () => done.fail("Unexpected relayout event"),
+            onRelayout: () => done.fail('Unexpected relayout event'),
           })
             .then(() => {
-              window.dispatchEvent(new Event("resize"));
+              window.dispatchEvent(new Event('resize'));
               setTimeout(done, 20);
             })
             .catch(err => done.fail(err));
